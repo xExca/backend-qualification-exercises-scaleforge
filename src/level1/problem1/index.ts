3481a5dd90779d764a7c18f94815b7aac31550b4
Onins <naustral@ncmh.gov.ph>
2025-11-12 11:21:00 +0800

    serialize

--- src/level1/problem1/index.ts
+++ src/level1/problem1/index.ts
@@ -7,11 +7,37 @@ export type Value = string | number | boolean | null | undefined |
  * compatible objects.
  */
 export function serialize(value: Value): unknown {
-  /**
-   * insert your code here
-   */
-  
-  return;
+  // This if statement catches the most of the primitive types and null
+  if(value === null || typeof value === "string" || typeof value === "number" || typeof value === "boolean" || value === undefined) {
+    return value;
+  }
+  // The if statement will check if the value is a Map, Set, Buffer
+  // This return a type of Map and value of an array
+  if(value instanceof Map) {
+    return { __t: "Map", __v: Array.from(value.entries()) };
+  }
+  if(value instanceof Set) {
+    return { __t: "Set", __v: Array.from(value) };
+  }
+  if(value instanceof Buffer) {
+    return { __t: "Buffer", __v: Array.from(value) };
+  }
+  // The return value of number in milliseconds
+  if(value instanceof Date) {
+    return { __t: "Date", __v: value.getTime() };
+  }
+
+  // If the value is an array it would map each and use the current function to check each type
+  if(Array.isArray(value)) { 
+    return value.map(serialize);
+  }
+  // Since other type is already filtered, this will check for the objects
+  // This will do a for loop and create a new object but check it with the current function
+  const out: Record<string, unknown> = {};
+  for (const [key, val] of Object.entries(value)) {
+    out[key] = serialize(val as Value);
+  }
+  return out;
 }
 
 /**
