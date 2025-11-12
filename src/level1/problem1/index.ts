3f34ca33f15ccc6d6a324658268e6c6ce66eccd7
Onins <naustral@ncmh.gov.ph>
2025-11-12 12:46:58 +0800

    deserialize

--- src/level1/problem1/index.ts
+++ src/level1/problem1/index.ts
@@ -45,9 +45,48 @@ export function serialize(value: Value): unknown {
  * scalar and objects.
  */
 export function deserialize<T = unknown>(value: unknown): T {
-  /**
-   * insert your code here
-   */
+  // This if statement catches the most of the primitive types
+  if(value === null || typeof value === "string" || typeof value === "number" || typeof value === "boolean" ||value === undefined) {
+    return value as T;
+  }
+  // val is a temporary variable
+  const val = value as any;
+
+  // It will check the map and deserialize each key and value pair
+  if(val.__t === "Map") {
+    return new Map(
+      val.__v.map(([k, val]: [unknown, unknown]) => [
+        deserialize(k),
+        deserialize(val),
+      ])
+    ) as T;
+  }
+
+  // It will check the set and deserialize each element
+  if(val.__t === "Set") {
+    return new Set(val.__v.map(deserialize)) as T;
+  }
+  // It will check the Buffer and deserialize
+  if(val.__t === "Buffer") {
+    return Buffer.from(val.__v) as T;
+  }
+  // If the type is a date
+  // It will check the date and deserialize
+  if(val.__t === "Date") {
+    return new Date(val.__v) as T;
+  }
+
+  // If the value is an array it will map each and use the current function to check each type
+  if(Array.isArray(val)) {
+    return val.map(deserialize) as T;
+  }
   
-  return;
+  // Since other type is already filtered, this will check for the objects
+  // This will do a for loop and create a new object but check it with the current function
+  const out: Record<string, unknown> = {};
+  for (const [key, value] of Object.entries(val)) {
+    out[key] = deserialize(value as unknown);
+  }
+  return out as T;
+  
 }
