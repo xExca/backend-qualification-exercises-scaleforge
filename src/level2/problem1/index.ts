a96e5984d57f48884de7b047e8ccf34d3ed8c692
Onins <naustral@ncmh.gov.ph>
2025-11-13 08:17:06 +0800

    cache

--- src/level2/problem1/index.ts
+++ src/level2/problem1/index.ts
@@ -1,11 +1,37 @@
 export class ExecutionCache<TInputs extends Array<unknown>, TOutput> {
+  private cache: Map<string, Promise<TOutput>> = new Map();
+  private pending: Map<string, Promise<TOutput>> = new Map();
+
   constructor(private readonly handler: (...args: TInputs) => Promise<TOutput>) {}
   
   async fire(key: string, ...args: TInputs): Promise<TOutput> {
-    /**
-     * insert your code here
-     */
-  
-    return;
+    // This will check if there is already existing data in the cache
+    if (this.cache.has(key)) {
+      return this.cache.get(key)!;
+    }
+
+    // This will check if there is a pending execution
+    const pending = this.pending.get(key);
+    if (pending) {
+      return pending;
+    }
+
+    const promise = this.handler(...args)
+      .then((result) => {
+        // When the successful execution is done, it will add the result to the cache
+        this.cache.set(key, Promise.resolve(result));
+        return result;
+      }).catch((error) => {
+        // this will catch any error when there is issue in handler
+        throw error;
+      }).finally(() => {
+        // This will execute every time when the promise is executed and delete the current cache
+        this.pending.delete(key);
+      })
+      
+    // This will set the result of the promise to the cache
+    this.pending.set(key, promise);
+    
+    return promise;
   }
 }
